f1c3d513ffdbcfba9ca67bc8945b1d81d5d55417
Marc Mezger <marc.mezger@gmail.com>
2023-05-17 16:46:21 +0200

    updating the config.

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  -- Lua
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        lazy = false,
+      }
+    end,
+  },
 }
